8e5711a802001c2140626bc768dfb6a6b3ba7c92
NIFLD <44540331+NIFLD@users.noreply.github.com>
2022-07-30 20:58:18 +0800

    update

--- Assets/player.cs
+++ Assets/player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class player : MonoBehaviour
 {
@@ -10,11 +12,15 @@ public class player : MonoBehaviour
 
     //身體部分
     [SerializeField] Transform snackbody ;    
+    [SerializeField] GameObject Startbutton ;
+    [SerializeField] GameObject Pausebutton ;
+    [SerializeField] GameObject Restartbutton ;
     List<Transform> _segments;
-    
+ 
     // Start is called before the first frame update
     void Start()
     {
+        StartGame();       
         currentvec = Vector3.right*MoveSpeed;
         _segments = new List<Transform>();
         _segments.Add(transform);
@@ -32,7 +38,6 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {                   
-        
         //控制
         if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
@@ -48,20 +53,48 @@ public class player : MonoBehaviour
             currentvec = Vector3.down*MoveSpeed;
         }
 
-
-        
-   
+        //Pause
+        if(Input.GetKeyDown(KeyCode.Space) && Time.timeScale ==1f ){
+            Time.timeScale=0f;
+            Pausebutton.SetActive(true);
+        }
     }          
     private void OnCollisionEnter2D(Collision2D other) {
-        if(other.transform.tag =="apple"){
+        if(other.transform.tag =="apple"){            
             Grow();
+        }else if(other.transform.tag == "wall"){
+            Die();
         }
     }
 
-    void Grow(){
+    private void Grow(){        
         Transform segment = Instantiate(snackbody);
         segment.position  = _segments[_segments.Count-1].position;
         _segments.Add(segment);
     }
-    
+
+    public void PlayGame(){     
+        var button = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;   
+        if(button.name == "Re"){            
+            SceneManager.LoadScene("SampleScene");
+            Restartbutton.SetActive(false);
+        }else{
+            Startbutton.SetActive(false);
+        }
+        Time.timeScale = 1f;
+        
+    }
+
+    public void PauseGame(){
+        Time.timeScale = 1f;
+        Pausebutton.SetActive(false);
+    }
+    public void Die(){
+        Time.timeScale = 0f;
+        Restartbutton.SetActive(true);
+    }
+    public void StartGame(){
+        Time.timeScale = 0f;
+        Startbutton.SetActive(true);
+    }
 }
