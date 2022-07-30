091986fdc90bba512879d27f8062e35a0e314a54
NIFLD <44540331+NIFLD@users.noreply.github.com>
2022-07-30 22:30:25 +0800

    update

--- Assets/player.cs
+++ Assets/player.cs
@@ -1,29 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
 public class player : MonoBehaviour
 {
-    
-    [SerializeField] float MoveSpeed = 2f;
+    //速度
+    [SerializeField] float MoveSpeed ;
+    [SerializeField] Text speed ;
+    [SerializeField] GameObject plus;
+    [SerializeField] GameObject de;
     Vector3 currentvec ;
 
     //身體部分
     [SerializeField] Transform snackbody ;    
+    List<Transform> _segments;
+    //遊戲系統
     [SerializeField] GameObject Startbutton ;
     [SerializeField] GameObject Pausebutton ;
     [SerializeField] GameObject Restartbutton ;
-    List<Transform> _segments;
- 
+
+    //分數相關
+    [SerializeField] Text n_score ;
+    int score ;
+    
     // Start is called before the first frame update
     void Start()
-    {
-        StartGame();       
-        currentvec = Vector3.right*MoveSpeed;
+    {       
+        Time.timeScale = 0f;
+        score = 0;  
+        MoveSpeed = 2f ;      
+        currentvec = Vector3.right;
         _segments = new List<Transform>();
         _segments.Add(transform);
+        speed.text = "SPEED:"+ ((int)MoveSpeed).ToString();
     }
 
     private void FixedUpdate() {
@@ -32,25 +44,26 @@ public class player : MonoBehaviour
         {
             _segments[i].position = _segments[i-1].position;
         }
-        transform.position += currentvec*Time.deltaTime;
+        transform.position += currentvec*MoveSpeed*Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {                   
+        
         //控制
         if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            currentvec = Vector3.left*MoveSpeed;
+            currentvec = Vector3.left;
         }else if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
-            currentvec = Vector3.right*MoveSpeed;
+            currentvec = Vector3.right;
         }else if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
-            currentvec = Vector3.up*MoveSpeed;
+            currentvec = Vector3.up;
         }else if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
-            currentvec = Vector3.down*MoveSpeed;
+            currentvec = Vector3.down;
         }
 
         //Pause
@@ -62,6 +75,7 @@ public class player : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.transform.tag =="apple"){            
             Grow();
+            UpdateScore(10);
         }else if(other.transform.tag == "wall"){
             Die();
         }
@@ -84,7 +98,6 @@ public class player : MonoBehaviour
         Time.timeScale = 1f;
         
     }
-
     public void PauseGame(){
         Time.timeScale = 1f;
         Pausebutton.SetActive(false);
@@ -97,4 +110,18 @@ public class player : MonoBehaviour
         Time.timeScale = 0f;
         Startbutton.SetActive(true);
     }
-}
+    void UpdateScore(int num){
+        score += num ;        
+        n_score.text = "score:"+ score.ToString();
+    }
+
+    public void ChangeSpeed(){
+        var s  = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;        
+        if(s == de){      
+            if(MoveSpeed>1f){MoveSpeed -= 1f;}            
+        }else if(s == plus ){            
+            if(MoveSpeed<8f){MoveSpeed += 1f;}            
+        }
+        speed.text = "SPEED:"+ ((int)MoveSpeed).ToString();
+    }
+}   
